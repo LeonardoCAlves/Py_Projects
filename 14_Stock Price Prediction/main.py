142680d62e5b8da4ec5e7221f096f7d70966bdd4
LeonardoCAlves <leon4rdoalvess@gmail.com>
2024-06-22 01:48:16 -0300

    feat: Implement initial data preparation and linear regression model training
    - Added data preparation function  to preprocess input data for forecasting:
      - Shifted forecast column to create labels
      - Scaled feature array using
      - Split data into training and test sets using
      - Extracted recent data for prediction
    
    - Loaded dataset from 'prices.csv' and defined parameters for forecast column, forecast horizon, and test size.
    
    - Initialized and trained a linear regression model using  from scikit-learn:
      - Fitted model on training data
      - Evaluated model performance on test data
      - Generated forecast for the next period
    
    - Created a response dictionary containing test score and forecasted values and printed the result.

---
+++ 14_Stock Price Prediction/main.py
@@ -0,0 +1,49 @@
+import numpy as np
+import pandas as pd
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+def prepare_data(df,forecast_col,forecast_out,test_size):
+    # creating new column called label with the last 5 rows are nan
+    label = df[forecast_col].shift(-forecast_out)
+
+    X = np.array(df[[forecast_col]]) # creating the feature array
+    X = preprocessing.scale(X)       # processing the feature array
+    
+    #creating the column i want to use later in the predicting method
+    X_lately = X[-forecast_out:] 
+
+    X = X[:-forecast_out]        # X that will contain the training and testing
+    label.dropna(inplace=True)   # dropping na values
+    y = np.array(label)          # assigning Y
+    
+    X_train, X_test, Y_train, Y_test = train_test_split(
+        X, y, test_size=test_size, random_state=0
+    ) #cross validation
+
+    response = [X_train,X_test , Y_train, Y_test , X_lately]
+    return response
+
+
+df = pd.read_csv("./data/prices.csv")
+
+forecast_col = 'Close'
+forecast_out = 5
+test_size = 0.2
+
+X_train, X_test, Y_train, Y_test , X_lately = prepare_data(
+    df,forecast_col,forecast_out,test_size
+); # calling the method were the cross validation and data preperation is in
+
+learner = LinearRegression() # initializing linear regression model
+
+learner.fit(X_train,Y_train) # training the linear regression model
+
+score=learner.score(X_test,Y_test)  # testing the linear regression model
+forecast= learner.predict(X_lately) # set that will contain the forecasted data
+response={}                         # creting json object
+response['test_score']=score
+response['forecast_set']=forecast
+
+print(response)
